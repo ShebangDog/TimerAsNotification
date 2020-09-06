a533df327d2c339df248965d4c08b16a150fa8d9
ShebangDog <e1n18088@oit.ac.jp>
2020-09-07 01:31:17 +0900

    viewBindingを使用する

--- app/src/main/java/dog/shebang/timer/MainActivity.kt
+++ app/src/main/java/dog/shebang/timer/MainActivity.kt
@@ -5,20 +5,26 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import dog.shebang.timer.Constants.CHANNEL_ID
+import dog.shebang.timer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         createNotificationChannel()
+
+        binding.apply {
+
+        }
     }
 
     override fun onStop() {
